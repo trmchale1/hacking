6ffa617e484413da9b9523f9c61fb83106173f26
Tim McHale <timmchale@Tims-MacBook-Pro.local>
2023-07-01 20:40:32 -0600

    added bandit 8

---
+++ bandit08.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+apt-get update
+apt-get install -y ssh
+apt-get install -y sshpass
+
+
+for LINE in $(cat pass.txt)
+do
+    pass_seven=$LINE
+done
+
+EIGHT_SCRIPT='grep millionth data.txt | cut -f 2'
+
+level_eight_pass=$(sshpass -p $pass_seven ssh -p 2220 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -X bandit7@bandit.labs.overthewire.org "$EIGHT_SCRIPT")
+
+echo $level_eight_pass >> "pass.txt"
