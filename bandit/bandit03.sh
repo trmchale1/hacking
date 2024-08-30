5c2d6b1955b69f4da6c003da005542d6944c6ee7
trmchale1 <trmchale@gmail.com>
2024-08-30 19:13:31 -0400

    update bandit03

--- bandit/bandit03.sh
+++ bandit/bandit03.sh
@@ -7,6 +7,6 @@ apt-get install -y sshpass
 
 pass_three="MNk8KNH3Usiio41PRUEoDFPqfxLPlSmx"
 
-level_four_pass=$(sshpass -p $pass_three ssh -p 2220 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -X bandit3@bandit.labs.overthewire.org 'cd inhere && cat .hidden')
+level_four_pass=$(sshpass -p $pass_three ssh -p 2220 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -X bandit3@bandit.labs.overthewire.org 'cd inhere && cat ...Hiding-From-You')
 
 echo $level_four_pass
--- bandit/bandit04.sh
+++ bandit/bandit04.sh
@@ -4,14 +4,9 @@ apt-get update
 apt-get install -y ssh
 apt-get install -y sshpass
 
-
-for LINE in $(cat pass.txt)
-do
-    pass_four=$LINE
-done
+pass_four="2WmrDFRmJIq3IPxneAaMGhap0pFhF3NJ"
 
 FIVE_SCRIPT="ls -p | grep -roh '^[A-Za-z0-9]*$'"
-
 
 level_three_pass=$(sshpass -p $pass_four ssh -p 2220 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -X bandit4@bandit.labs.overthewire.org "cd inhere && $FIVE_SCRIPT")
 
