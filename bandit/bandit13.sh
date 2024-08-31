31f946d1a9b943fac27779bd1bf39858baa9c93b
trmchale1 <trmchale@gmail.com>
2024-08-31 19:18:42 -0400

    add bandit14

--- bandit/bandit13.sh
+++ bandit/bandit13.sh
@@ -5,3 +5,5 @@ apt-get install -y ssh
 apt-get install -y sshpass
 
 pass_thirteen="FO5dwFsc0cbaIiH0h8J2eUks2vdTDwAn"
+
+# run scp -P 2220 bandit13@bandit.labs.overthewire.org:sshkey.private .
---
+++ bandit/bandit14.sh
@@ -0,0 +1,3 @@
+# using sshkey.private, change its perms 
+# chmod 700 sshkey.private
+# run ssh -i sshkey.private bandit14@bandit.labs.overthewire.org -p 2220
