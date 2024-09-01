6e8371006f313b46cf11dbf9b54f2f9eeea6e444
trmchale1 <trmchale@gmail.com>
2024-09-01 15:11:22 -0400

    completed 16 thru 18

---
+++ bandit/bandit17.sh
@@ -0,0 +1,3 @@
+# ssh -i sshkey17.private bandit17@bandit.labs.overthewire.org -p 2220
+
+# diff passwords.old passwords.new
---
+++ bandit/bandit18.sh
@@ -0,0 +1,7 @@
+# x2gLTTjFwMOhQ8oWNbMN362QKxfRqGlO
+
+# you are automatically logged out
+
+# ssh bandit18@bandit.labs.overthewire.org -p 2220 cat readme
+
+# cGWpMaKXVwDUNgPAVJbWYuGHVn9zl3j8
