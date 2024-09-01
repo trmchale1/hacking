520834648a573bce9e13d2533eaf0ff6cc51c192
trmchale1 <trmchale@gmail.com>
2024-09-01 13:22:18 -0400

    finished bandit15

--- bandit/bandit14.sh
+++ bandit/bandit14.sh
@@ -1,3 +1,6 @@
 # using sshkey.private, change its perms 
 # chmod 700 sshkey.private
 # run ssh -i sshkey.private bandit14@bandit.labs.overthewire.org -p 2220
+# on login run cat /etc/bandit_pass/bandit14
+# nc localhost 30000
+# 8xCjnmgoKbGLhHFAZlGE5Tmu4M2tKJQo
--- bandit/bandit15.sh
+++ bandit/bandit15.sh
@@ -1,1 +1,8 @@
-# MU4VWeTyJk8ROof1qqmcBPaLh7lDCPvS
+# 8xCjnmgoKbGLhHFAZlGE5Tmu4M2tKJQo
+
+# bandit15@bandit:~$ openssl s_client -connect localhost:30001
+#
+#8xCjnmgoKbGLhHFAZlGE5Tmu4M2tKJQo
+#Correct!
+#kSkvUpMQ7lBYyCM4GBPvCvT1BfWRy0Dx
+
