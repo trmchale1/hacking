002c1bbd46f352d29dc96afe98cf08ce9cc1e370
trmchale1 <trmchale@gmail.com>
2024-09-01 16:05:26 -0400

    adding bandit20 and bandit21

---
+++ bandit/bandit20.sh
@@ -0,0 +1,10 @@
+# 0qXahG8ZjOVMN9Ghs7iOWsCfZyXOUbYO
+
+#bandit20@bandit:~$ echo -n '0qXahG8ZjOVMN9Ghs7iOWsCfZyXOUbYO' | nc -l -p 1234 &
+#[1] 1207977
+#bandit20@bandit:~$ ./suconnect 1234
+#Read: 0qXahG8ZjOVMN9Ghs7iOWsCfZyXOUbYO
+#Password matches, sending next password
+
+
+# EeoULMCra2q0dSkYj561DX7s1CpBuOBt
