e297eeae004dc06772513d74f06b7067035b5428
trmchale1 <trmchale@gmail.com>
2024-09-02 12:11:27 -0400

    add bandit25

--- bandit/bandit24.sh
+++ bandit/bandit24.sh
@@ -1,2 +1,22 @@
 # gb8KRRCsshuZXI0tUuR6ypOFjiZbf3G8
 
+#bandit24@bandit:~$ nc localhost 30002
+#I am the pincode checker for user bandit25. Please enter the password for user bandit24 and the secret pincode on a single line, separated by a space.
+#x
+#Wrong! Please enter the correct current password and pincode. Try again.
+#^C
+#bandit24@bandit:~$ mktemp -d
+#/tmp/tmp.Umd2JhNSOU
+#bandit24@bandit:~$ cd /tmp/tmp.Umd2JhNSOU
+#bandit24@bandit:/tmp/tmp.Umd2JhNSOU$ vim brute_force_pin.sh
+#bandit24@bandit:/tmp/tmp.Umd2JhNSOU$ chmod +x brute_force_pin.sh
+#bandit24@bandit:/tmp/tmp.Umd2JhNSOU$ ./brute_force_pin.sh
+#bandit24@bandit:/tmp/tmp.Umd2JhNSOU$ ls
+#brute_force_pin.sh  possibilities.txt  result.txt
+#bandit24@bandit:/tmp/tmp.Umd2JhNSOU$ sort result.txt | grep -v "Wrong!"
+#
+#Correct!
+#I am the pincode checker for user bandit25. Please enter the password for user bandit24 and the secret pincode on a single line, separated by a space.
+#The password of user bandit25 is iCi86ttT4KSNe1armKiwbQNmB3YJP3q4
+
+# iCi86ttT4KSNe1armKiwbQNmB3YJP3q4
